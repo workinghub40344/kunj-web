d5ad7a4ca60e678ee41bfe1e5ed86d6ff8741915
workinghub40344 <manishsharma40344@gmail.com>
2025-09-20 15:39:21 +0530

    done

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect Database
+connectDB();
+
+// Routes (example)
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
