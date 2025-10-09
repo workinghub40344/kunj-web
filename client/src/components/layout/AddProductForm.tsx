93fa0bf2db02222f2f1dc408c0936fe6deaa9d7b
workinghub40344 <manishsharma40344@gmail.com>
2025-10-09 13:16:03 +0530

    done

--- client/src/components/layout/AddProductForm.tsx
+++ client/src/components/layout/AddProductForm.tsx
@@ -480,8 +480,3 @@ const AddProductForm = ({
 };
 
 export default AddProductForm;
-
-
-
-
-const sizeArr = [ "1-no", "2-no", "3-no", "4-no", "5-no", "6-no", "7-no", "8-no", "9-no", "10-no", "11-no", "12-no", "13-no", "14-no", "15-no", "16-no", "17-no", "18-no", "19-no", "20-no", "21-no", "22-no", "23-no", "24-no", "25-no", "26-no", "27-no", "28-no", "29-no", "30-no", "1-inch", "2-inch", "3-inch", "4-inch", "5-inch", "6-inch", "7-inch", "8-inch", "9-inch", "10-inch", "11-inch", "12-inch", "13-inch", "14-inch", "15-inch", "16-inch", "17-inch", "18-inch", "19-inch", "20-inch", "21-inch", "22-inch", "23-inch", "24-inch", "25-inch", "26-inch", "27-inch", "28-inch", "29-inch", "30-inch", ]
